5429b9fa4914ab6b91a1738b1c4e45c05f369483
shourya nagpal <code4shourya@gmail.com>
2020-10-20 02:25:25 +0530

    Added time complexity and space complexity analysis in comments

--- countingSort.cpp
+++ countingSort.cpp
@@ -25,6 +25,17 @@ void countingSort(int arr[] , int n){
 }
 
 int main(){
+    // Time Complexity :
+    // worst  : Big-O(n+k)
+    // best   : Theita(n+k>)
+    // average: Omega(n+k)
+
+    // In Place - No
+    // Stable   - Yes
+    // Space Complexity :n+r
+    // Non Comparison Sort
+    //Wort case performance : where k is the range of the non-negative key values
+	
     int arr[] = {10,9,8,7,6,5,4,3,2,11,2,3} ;
     int m = *max_element(arr,arr+10);
     cout<<m;
